17a548714f5b04431ab53fa788c1f0839aa00372
Dallan Prince <dallan.prince@gmail.com>
2022-10-09 22:08:18 -0500

    Integrated comments (initial blueprint) into code

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# SECRETS Game
+# by Leah, Hannah, and Caleb Prince
+# assisted by Dallan
+# 2022-10-09
+
 import random
 import time
 
 # list of trusted people
-people = ['holly', 'madison', 'dallan', 'carrie', 'hannah', 'evangeline', 'phoenix', 'caleb', 'leah', 'kambria', 'kinsley']
+people = ['holly', 'madison', 'dallan', 'carrie', 'hannah', 'evangeline', 'phoenix', 'caleb', 'leah', 'kambria', 'kinzley']
 
 # list of secrets
 secret_list = [
@@ -23,6 +28,7 @@ def secrets():
         time.sleep(0.5)
         print("You are in our special list, we have a secret for you!")
         time.sleep(0.5)
+        # if name is in list tell a secret
         print(random.choice(secret_list))
         time.sleep(5)
         player_secret = input("If you would like to hear another secret, type in a secret, otherwise just press Enter to quit: ")
@@ -32,9 +38,8 @@ def secrets():
         else:
             print("That's okay. Come back and play again later")
     else:
+        # else say sorry we can't help you
         print("Sorry we can't help you.")
-    # if name is in list tell a secret
 
-    # else say sorry we can't help you
 
 
