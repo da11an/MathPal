ac1a17f42d1a840ddd9b7ef7baf68aed91f8dd8d
Dallan Prince <dallan.prince@gmail.com>
2022-10-16 00:02:28 -0500

    adding math module

--- secrets.py
+++ secrets.py
@@ -7,7 +7,19 @@ import random
 import time
 
 # list of trusted people
-people = ['holly', 'madison', 'dallan', 'carrie', 'hannah', 'evangeline', 'phoenix', 'caleb', 'leah', 'kambria', 'kinzley']
+people = [
+    'holly',
+    'madison',
+    'dallan',
+    'carrie',
+    'hannah',
+    'evangeline',
+    'phoenix',
+    'caleb',
+    'leah',
+    'kambria',
+    'kinzley'
+    ]
 
 # list of secrets
 secret_list = [
@@ -15,25 +27,34 @@ secret_list = [
     "Leah's birthday is January 16",
     "we like to say weird words",
     "we like to call Daddy Doodleluxadisiousiousiousiousious",
-    "our mother is pregnant with a boy"]
+    "our mother is pregnant with a boy",
+    "I have two cats",
+    "One of my cats has this weird bump on her head",
+    "I had a ferret when I was 4"]
 
 
 def secrets():
 
     # what is your name
     player = input("what is your name? ")
+
+    # greet the player
+    time.sleep(1)
+    print("Hello " + player)
     
     # check if name is in list
     if player.lower() in people:
-        time.sleep(0.5)
+        time.sleep(1)
         print("You are in our special list, we have a secret for you!")
-        time.sleep(0.5)
+        time.sleep(1)
         # if name is in list tell a secret
         print(random.choice(secret_list))
-        time.sleep(5)
+        time.sleep(1)
         player_secret = input("If you would like to hear another secret, type in a secret, otherwise just press Enter to quit: ")
+        time.sleep(1)
         if len(player_secret.split()) >= 3:
             print("That sounds like a secret, here is one of ours:")
+            time.sleep(1)
             print(random.choice(secret_list))
         else:
             print("That's okay. Come back and play again later")
@@ -43,3 +64,5 @@ def secrets():
 
 
 
+if __name__ == "__main__":
+    secrets()
---
+++ try_math.py
@@ -0,0 +1,216 @@
+import random
+import time
+import json
+
+
+def login():
+    player_log = read_log()
+    player_list = list(player_log.keys())
+    if type(player_list) != list:
+        player_list = [player_list]
+    player = let_user_pick(options = ["Add me, I'm new"] + player_list)
+    if player == "Add me, I'm new":
+        player = input("What's your name: ")
+        if len(player) > 2:
+            add_player(player)
+            session = 1
+        else:
+            print("Please choose a name with at least 3 characters, you may continue as a guest")
+            player = "Guest"
+    else: # player stats!!!     
+        print("")
+        print(f'Welcome back {player}!')
+        session = player_log[player]['session'][-1] + 1
+        last_seen = duration_str(time.time() - player_log[player]['timestamp'][-1])
+        print(f'It has been {last_seen} since you played.')
+        print("")
+        time.sleep(1)
+        print(f"Here's what you were working on:")
+        print("")
+        [print(player_log[player]['question'][i]) for i in range(len(player_log[player]['session'])) if player_log[player]['session'][i] == (session -1)]
+        print("")
+        time.sleep(1)
+
+    return player, session
+
+def duration_str(duration):
+    duration = round(duration)
+    if duration < 60:
+        return  f'{int(duration)} seconds'
+    elif duration < 3600:
+        seconds = duration % 60
+        minutes = (duration - seconds)/60
+        return f'{int(minutes)} minutes and {int(seconds)} seconds'
+    elif duration < 3600*24:
+        dur_min = round(duration/60)
+        minutes = dur_min % 60
+        hours = (dur_min - minutes)/60
+        return f'{int(hours)} hours and {int(minutes)} minutes'
+    else:
+        dur_hr = round(duration/3600)
+        hours = dur_hr % 24
+        days = (dur_hr - hours)/24
+        return f'{int(days)} days and {int(hours)} hours'
+
+# read JSON log
+def read_log(filename='data.json'):
+    with open(filename,'r+') as file:
+        return json.load(file)
+    
+def add_player(player, filename='data.json'):
+    print(f'Player {player} is being added')
+    with open(filename,'r+') as file:
+        file_data = json.load(file)
+        file_data[player] = {
+            "question": [],
+            "correct": [],
+            "timestamp": [],
+            "duration": [],
+            "session": []
+        }
+        file.seek(0)
+        json.dump(file_data, file, indent = 4)
+
+# function to add to JSON
+def log_result(player, question, correct, timestamp, duration, session, filename='data.json'):
+    with open(filename,'r+') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data[player]['question'].append(question)
+        file_data[player]['correct'].append(correct)
+        file_data[player]['timestamp'].append(timestamp)
+        file_data[player]['duration'].append(duration)
+        file_data[player]['session'].append(session)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+ 
+def encouragement(answer = True):
+    right = [
+        "Good job!",
+        "You're a rock star!",
+        "Fantastic!",
+        "Keep going!"
+    ]
+    wrong = [
+        "Better luck next time!",
+        "Don't give up!",
+        "Keep going!",
+        "You've got this",
+        "Nice try",
+        "Yarshiblah, keep trying",
+        "Blouwers, blouwers, hasai, hasai, don't give up",
+        "Gosh I feel so bad for you right now a-ho a-ho a-ho",
+        "Stink, sigh, blouwers",
+        "I smell something fishy, learn from failure",
+        "You just dropped a stinker, keep working",
+        "If you're never wrong, you're not learning"
+    ]
+    if answer:
+        return random.choice(right)
+    else:
+        return random.choice(wrong)
+
+def start_game():
+    player, session = login()
+    subject = let_user_pick(options = ['addition', 'subtraction', 'multiplication'])
+    n_problems = int_input('How many problems do you want to try: ')
+    max_value = int_input("What's the biggest number you want to try: ")
+    questions = []
+    score_card = []
+    time_card = []
+    dates = []
+    spacer = '+---------------------------------------+'
+    print(spacer)
+    for i in range(n_problems):
+        print(f'({i+1})')        
+        
+        start_time = time.time()
+        if subject == "addition":
+            identical, help_str = addition(max_value)
+        elif subject == "subtraction":
+            identical, help_str = subtraction(max_value)
+        else:
+            identical, help_str = multiplication(max_value)
+        
+        duration = time.time() - start_time
+        dates.append(time.time())
+        questions.append(help_str)
+        time_card.append(duration)
+        score_card.append(identical)
+
+        if identical:
+            print(f'Correct! {encouragement(identical)}')
+        else:
+            print(f'Actually, {help_str} {encouragement(identical)}')
+        print("")
+
+        log_result(
+            player = player,
+            question = help_str,
+            correct = identical,
+            timestamp = time.time(),
+            duration = duration,
+            session = session
+        )
+    
+    print(spacer)
+    time.sleep(1)
+    score = round(sum(score_card)/len(score_card) * 100)
+    print(f'Fun Game! You got {sum(score_card)}/{len(score_card)} correct = {score}%')
+    print(f'Your average answer time was {round(sum(time_card)/len(time_card), 1)} seconds')
+    print(f'Your slowest answer time was {round(max(time_card), 1)} seconds')
+    print(f'Your fastest answer time was {round(min(time_card), 1)} seconds')
+    print(spacer)
+    print("")
+    return(score)
+
+
+def multiplication(biggest_number = 12):
+    a = random.randint(1, biggest_number)
+    b = random.randint(1, biggest_number)
+    answer = int_input(f'          {a} * {b} = ')
+    identical = answer == (a*b)
+    return identical, f'{a} * {b} = {a*b}'
+
+def addition(biggest_number = 12):
+    a = random.randint(1, biggest_number)
+    b = random.randint(1, biggest_number)
+    answer = int_input(f'          {a} + {b} = ')
+    identical = answer == (a+b)
+    return identical, f'{a} + {b} = {a+b}'
+
+def subtraction(biggest_number = 12):
+    a = random.randint(1, biggest_number)
+    b = random.randint(1, biggest_number)
+    if b > a:
+        a, b = b, a
+    answer = int_input(f'          {a} - {b} = ')
+    identical = answer == (a-b)
+    return identical, f'{a} - {b} = {a-b}'
+
+def int_input(prompt = ""):
+    inp = input(prompt)       # Get the input
+    while not inp.isdigit():
+        print("Wups! Please try a whole number.")
+        inp = input(prompt)
+    return int(inp)
+
+def let_user_pick(options):
+    print("Please choose:")
+
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx + 1, element))
+
+    i = int_input("Enter number: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return options[int(i) - 1]
+    except:
+        pass
+    return None
+
+if __name__ == "__main__":
+    start_game()
